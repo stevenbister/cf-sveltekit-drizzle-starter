75c636a5125b571653e581e4097dd1cddc27e5ec
Steven Bister <stevebister@gmail.com>
2024-11-07 20:03:04 +0000

    add auth setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,19 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	testDir: 'e2e',
+	projects: [
+		{
+			name: 'setup',
+			testMatch: /.*\.setup\.ts/
+		},
+		{
+			name: 'tests',
+			use: {
+				storageState: 'playwright/.auth/session.json'
+			},
+			dependencies: ['setup']
+		}
+	],
 	use: {
 		baseURL: process.env.CI ? process.env.PLAYWRIGHT_TEST_BASE_URL : 'http://localhost:8788'
 	},
