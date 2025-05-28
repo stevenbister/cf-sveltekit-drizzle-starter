08743e91178c7bb1894baedfd68bcd8e5ce697b6
Steven Bister <stevebister@gmail.com>
2025-05-28 15:58:34 +0100

    update user mocks

--- mocks/user.ts
+++ mocks/user.ts
@@ -1,5 +1,28 @@
-export const mockUser = {
+import type { Account } from '$lib/server/db/schema/account';
+import type { User } from '$lib/server/db/schema/user';
+
+export const mockUser: User = {
+	id: '1234',
 	email: 'john@doe.com',
+	name: 'John Doe',
+	emailVerified: true,
+	image: null,
+	createdAt: new Date(),
+	updatedAt: new Date()
+};
+
+export const mockAccount: Account = {
+	id: '5678',
+	accountId: 'account1234',
+	providerId: 'provider1234',
+	userId: mockUser.id,
 	password: 'password',
-	name: 'John Doe'
+	accessToken: null,
+	refreshToken: null,
+	idToken: null,
+	accessTokenExpiresAt: null,
+	refreshTokenExpiresAt: null,
+	scope: null,
+	createdAt: new Date(),
+	updatedAt: new Date()
 };
