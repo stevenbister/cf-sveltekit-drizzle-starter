cd6000c662be0b19ad674749f8b582004859fab3
Steven Bister <stevebister@gmail.com>
2025-05-29 17:00:06 +0100

    install bits ui and add example component

---
+++ src/lib/components/button/button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	// https://www.bits-ui.com/docs/components/button
+	import type { ButtonRootProps } from 'bits-ui';
+	import { Button } from 'bits-ui';
+
+	let { children, ...rest }: ButtonRootProps = $props();
+</script>
+
+<Button.Root {...rest}>{@render children?.()}</Button.Root>
